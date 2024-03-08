87902df73f45dc3ef351c16b085fd4ce9edbfda0
Zain Kergaye <zain4utah@gmail.com>
2024-03-08 01:44:42 -0700

    Added angularls for angular dev

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ This is my fork of the NvChad config files.
 ## Installing:
 Remove the custom folder after installing NvChad, 
 then clone this repo there. 
+
+## Note: 
+### For angularls:
+All projects must have package language server from angular. Install via this command: npm i install @angular/language-server@16.2.0
--- plugins.lua
+++ plugins.lua
@@ -67,6 +67,16 @@ local plugins = {
     -- version = "*" 
     ft = { "java" },
   },
+    {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    ft = { "java" }, -- TODO Comments only for .java files
+    opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+        }
+    }
 }
 
 return plugins
