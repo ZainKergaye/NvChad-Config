d6fe9fad623105e2b765e7923f8d6d4d9a58c05a
Zain Kergaye <zain4utah@gmail.com>
2024-04-19 22:10:12 -0600

    Added config to todo-comments plugin

--- plugins.lua
+++ plugins.lua
@@ -3,129 +3,133 @@ local overrides = require "custom.configs.overrides"
 ---@type NvPluginSpec[]
 local plugins = {
 
-  -- Override plugin definition options
+    -- Override plugin definition options
 
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      require "plugins.configs.lspconfig"
-      require "custom.configs.lspconfig"
-    end, -- Override to setup mason-lspconfig
-  },
-
-  -- override plugin configs
-  {
-    "williamboman/mason.nvim",
-    opts = overrides.mason,
-  },
-
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = overrides.treesitter,
-  },
-
-  {
-    "nvim-tree/nvim-tree.lua",
-    opts = overrides.nvimtree,
-  },
-
-  -- Install a plugin
-  {
-    "max397574/better-escape.nvim",
-    event = "InsertEnter",
-    config = function()
-      require("better_escape").setup()
-    end,
-  },
-
-  {
-    "stevearc/conform.nvim",
-    --  for users those who want auto-save conform + lazyloading!
-    -- event = "BufWritePre"
-    config = function()
-      require "custom.configs.conform"
-    end,
-  },
-
-  -- To make a plugin not be loaded
-  -- {
-  --   "NvChad/nvim-colorizer.lua",
-  --   enabled = false
-  -- },
-
-  -- All NvChad plugins are lazy-loaded by default
-  -- For a plugin to be loaded, you will need to set either `ft`, `cmd`, `keys`, `event`, or set `lazy = false`
-  -- If you want a plugin to load on startup, add `lazy = false` to a plugin spec, for example
-  -- {
-  --   "mg979/vim-visual-multi",
-  --   lazy = false,
-  -- }
-  {
-    "danymat/neogen",
-    config = true,
-    -- Uncomment next line if you want to follow only stable versions
-    -- version = "*"
-    ft = { "java" },
-  },
-  {
-    "folke/todo-comments.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    ft = { "java", "html", "scss", "md" },
-    opts = {
-      -- your configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            require "plugins.configs.lspconfig"
+            require "custom.configs.lspconfig"
+        end, -- Override to setup mason-lspconfig
     },
-  },
 
-  {
-    "sbdchd/neoformat", -- Formatting for all files aside from java
-    lazy = false,
-  },
-
-  {
-    "tpope/vim-fugitive", -- Git integration
-    lazy = false,
-  },
-
-  {
-    "nvim-java/nvim-java",
-    lazy = false,
-    dependencies = {
-      "nvim-java/lua-async-await",
-      "nvim-java/nvim-java-core",
-      "nvim-java/nvim-java-test",
-      "nvim-java/nvim-java-dap",
-      "MunifTanjim/nui.nvim",
-      "neovim/nvim-lspconfig",
-      "mfussenegger/nvim-dap",
-      "rcarriga/nvim-dap-ui",
-      "nvim-neotest/nvim-nio",
-      {
+    -- override plugin configs
+    {
         "williamboman/mason.nvim",
-        opts = {
-          registries = {
-            "github:nvim-java/mason-registry",
-            "github:mason-org/mason-registry",
-          },
-        },
-      },
+        opts = overrides.mason,
     },
-    config = function()
-      require("java").setup {}
-      require("lspconfig").jdtls.setup {
-        on_attach = require("plugins.configs.lspconfig").on_attach,
-        capabilities = require("plugins.configs.lspconfig").capabilities,
-        filetypes = { "java" },
-      }
-    end,
-  },
-  {
-    "https://git.sr.ht/~swaits/thethethe.nvim",
-    lazy = true,
-    opts = {},
-    filetypes = { "adoc" },
-  },
+
+    {
+        "nvim-treesitter/nvim-treesitter",
+        opts = overrides.treesitter,
+    },
+
+    {
+        "nvim-tree/nvim-tree.lua",
+        opts = overrides.nvimtree,
+    },
+
+    -- Install a plugin
+    {
+        "max397574/better-escape.nvim",
+        event = "InsertEnter",
+        config = function()
+            require("better_escape").setup()
+        end,
+    },
+
+    {
+        "stevearc/conform.nvim",
+        --  for users those who want auto-save conform + lazyloading!
+        -- event = "BufWritePre"
+        config = function()
+            require "custom.configs.conform"
+        end,
+    },
+
+    -- To make a plugin not be loaded
+    -- {
+    --   "NvChad/nvim-colorizer.lua",
+    --   enabled = false
+    -- },
+
+    -- All NvChad plugins are lazy-loaded by default
+    -- For a plugin to be loaded, you will need to set either `ft`, `cmd`, `keys`, `event`, or set `lazy = false`
+    -- If you want a plugin to load on startup, add `lazy = false` to a plugin spec, for example
+    -- {
+    --   "mg979/vim-visual-multi",
+    --   lazy = false,
+    -- }
+    {
+        "danymat/neogen",
+        config = true,
+        -- Uncomment next line if you want to follow only stable versions
+        -- version = "*"
+        ft = { "java" },
+    },
+    {
+        "folke/todo-comments.nvim",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        ft = { "java", "html", "scss", "md" , "adoc"},
+        opts = {
+            keywords = {
+                HELP = {
+                    icon = "󰋖 ",
+                    color = "#D72F2F",
+                    alt = {"CONFUSED", "WEIRD"},
+                },
+            },
+        },
+    },
+
+    {
+        "sbdchd/neoformat", -- Formatting for all files aside from java
+        lazy = false,
+    },
+
+    {
+        "tpope/vim-fugitive", -- Git integration
+        lazy = false,
+    },
+
+    {
+        "nvim-java/nvim-java",
+        lazy = false,
+        dependencies = {
+            "nvim-java/lua-async-await",
+            "nvim-java/nvim-java-core",
+            "nvim-java/nvim-java-test",
+            "nvim-java/nvim-java-dap",
+            "MunifTanjim/nui.nvim",
+            "neovim/nvim-lspconfig",
+            "mfussenegger/nvim-dap",
+            "rcarriga/nvim-dap-ui",
+            "nvim-neotest/nvim-nio",
+            {
+                "williamboman/mason.nvim",
+                opts = {
+                    registries = {
+                        "github:nvim-java/mason-registry",
+                        "github:mason-org/mason-registry",
+                    },
+                },
+            },
+        },
+        config = function()
+            require("java").setup {}
+            require("lspconfig").jdtls.setup {
+                on_attach = require("plugins.configs.lspconfig").on_attach,
+                capabilities = require("plugins.configs.lspconfig").capabilities,
+                filetypes = { "java" },
+            }
+        end,
+    },
+    {
+        "https://git.sr.ht/~swaits/thethethe.nvim",
+        lazy = true,
+        opts = {},
+        filetypes = { "adoc" },
+    },
 }
 
 return plugins
